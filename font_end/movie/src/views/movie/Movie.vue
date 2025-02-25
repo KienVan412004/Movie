f91734b7ebe599c6bfbd50a0431f287f38f552d4
Kien Nguyen Van <kienvan131203@gmail.com>
2025-02-25 17:38:59 +0700

    update giao diện màn quản lý phim

--- font_end/movie/src/views/movie/Movie.vue
+++ font_end/movie/src/views/movie/Movie.vue
@@ -1,26 +1,59 @@
 <template>
   <Menu>
     <div class="movie">
-      <h1>Quản lý Phim</h1>
-      <div style="min-height: 580px; background-color: white; border-radius: 30px">
+      <!-- <h1>Quản lý Phim</h1> -->
+       <div>
+        <nav class="flex" aria-label="Breadcrumb">
+  <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+    <li class="inline-flex items-center">
+      <a href="#" class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
+        <svg class="w-3 h-3 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+          <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z"/>
+        </svg>
+        Home
+      </a>
+    </li>
+    <li>
+      <div class="flex items-center">
+        <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+        </svg>
+        <a href="#" class="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white">Projects</a>
+      </div>
+    </li>
+    <li aria-current="page">
+      <div class="flex items-center">
+        <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+        </svg>
+        <span class="ms-1 text-sm font-medium text-gray-500 md:ms-2 dark:text-gray-400">Flowbite</span>
+      </div>
+    </li>
+  </ol>
+</nav>
+       </div>
+      <div style="min-height: 580px;background-color: white; border-radius: 30px; margin-top: 30px;">
         <div
           style="
-            height: 60px;
+            height: 80px;
             width: 100%;
-            border-bottom: 1px solid grey;
+            border-bottom: 1px solid #EBEDF3;
             display: flex;
             align-items: center;
             padding-left: 30px;
           "
         >
-          <span style="font-size: 20px; font-weight: bold"> Quản lý Phim</span>
+        <img src="https://cdn-icons-png.flaticon.com/256/5362/5362466.png" alt="imade" style="width: 50px;height: 50px;">
+          <span style="font-size: 30px; font-weight: 600"> Quản lý Phim</span>
         </div>
         <div class="container mx-auto px-6 pt-10">
-          <div style="grid-template-columns: 28% 16% 28% 28%" class="grid gap-4">
+          <!-- style="grid-template-columns: 25% 22% 31% 19%" -->
+          <div  class="grid grid-cols-1 md:grid-cols-4 gap-4">
             <div>
               <input
                 type="text"
-                style="height: 30px"
+                style="height: 38px; border-radius: 12px; font-weight: 600;"
+                placeholder="Tìm kiếm"
                 id="large-input"
                 class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 s
@@ -44,6 +77,7 @@
                 </div>
                 <input
                   id="datepicker-title"
+                  style="border-radius: 12px;"
                   datepicker
                   datepicker-title="Flowbite datepicker"
                   type="text"
@@ -68,6 +102,7 @@
                 </div>
                 <input
                   id="datepicker-title"
+                  style="border-radius: 12px;"
                   datepicker
                   datepicker-title="Flowbite datepicker"
                   type="text"
@@ -76,17 +111,13 @@
                 />
               </div>
             </div>
-            <div>
-              <input
-                type="text"
-                style="height: 30px; width: 85%"
-                id="large-input"
-                class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                s
-              />
+            <div  style="display: flex; justify-content: end;">
+              <button class="btn-add" type="button">Thêm Mới</button>
             </div>
           </div>
         </div>
+
+
         <div class="container mx-auto px-6 pt-10">
           <div class="relative overflow-x-auto">
             <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
@@ -94,51 +125,71 @@
                 class="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400"
               >
                 <tr>
-                  <th scope="col" class="px-6 py-3 rounded-s-lg">Product name</th>
-                  <th scope="col" class="px-6 py-3">Qty</th>
-                  <th scope="col" class="px-6 py-3 rounded-e-lg">Price</th>
+                  <th scope="col" class="px-6 py-3">STT</th>
+                  <th scope="col" class="px-6 py-3 rounded-s-lg">TÊN PHIM</th>
+                  <th scope="col" class="px-6 py-3">THỂ LOẠI</th>
+                  <th scope="col" class="px-6 py-3 rounded-e-lg">NGÀY PHÁT HÀNH</th>
+                  <th scope="col" class="px-6 py-3">TRẠNG THÁI</th>
+                  <th scope="col" class="px-6 py-3 rounded-e-lg">HÀNH ĐỘNG</th>
                 </tr>
               </thead>
               <tbody>
-                <tr class="bg-white dark:bg-gray-800">
-                  <th
-                    scope="row"
-                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                  >
-                    Apple MacBook Pro 17"
-                  </th>
+                <tr v-for="(item, index) in 6" :key="index" class="bg-white dark:bg-gray-800">
                   <td class="px-6 py-4">1</td>
-                  <td class="px-6 py-4">$2999</td>
-                </tr>
-                <tr class="bg-white dark:bg-gray-800">
-                  <th
-                    scope="row"
-                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                  >
-                    Microsoft Surface Pro
-                  </th>
-                  <td class="px-6 py-4">1</td>
-                  <td class="px-6 py-4">$1999</td>
-                </tr>
-                <tr class="bg-white dark:bg-gray-800">
-                  <th
-                    scope="row"
-                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                  >
-                    Magic Mouse 2
-                  </th>
-                  <td class="px-6 py-4">1</td>
-                  <td class="px-6 py-4">$99</td>
+                <th
+                 scope="row"
+                 class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                >
+                            Apple MacBook Pro 17" - {{ index + 1 }}
+                </th>
+                   <td class="px-6 py-4">1</td>
+                   <td class="px-6 py-4">$2999</td>
+                   <td class="px-6 py-4">1</td>
+                   <td class="px-6 py-4">
+                    <button class="btn-view mx-2" type="button"></button>
+                    <button class="btn-update mx-2" type="button"></button>
+                    <button class="btn-delete mx-2" type="button"></button>
+                   </td>
                 </tr>
               </tbody>
-              <tfoot>
-                <tr class="font-semibold text-gray-900 dark:text-white">
-                  <th scope="row" class="px-6 py-3 text-base">Total</th>
-                  <td class="px-6 py-3">3</td>
-                  <td class="px-6 py-3">21,000</td>
-                </tr>
-              </tfoot>
             </table>
+            <div style="display: flex; justify-content: end" class="mx-auto px-6 py-6" >
+              <nav aria-label="Page navigation example">
+  <ul class="flex items-center -space-x-px h-8 text-sm">
+    <li>
+      <a href="#" class="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+        <span class="sr-only">Previous</span>
+        <svg class="w-2.5 h-2.5 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
+        </svg>
+      </a>
+    </li>
+    <li>
+      <a href="#" class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">1</a>
+    </li>
+    <li>
+      <a href="#" class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">2</a>
+    </li>
+    <li>
+      <a href="#" aria-current="page" class="z-10 flex items-center justify-center px-3 h-8 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">3</a>
+    </li>
+    <li>
+      <a href="#" class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">4</a>
+    </li>
+    <li>
+      <a href="#" class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">5</a>
+    </li>
+    <li>
+      <a href="#" class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+        <span class="sr-only">Next</span>
+        <svg class="w-2.5 h-2.5 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+        </svg>
+      </a>
+    </li>
+  </ul>
+</nav>
+            </div>
           </div>
         </div>
       </div>
@@ -148,4 +199,5 @@
 
 <script setup lang="ts">
 import Menu from '../../layout/Menu.vue'
+import "../../assets/movie.css"
 </script>
